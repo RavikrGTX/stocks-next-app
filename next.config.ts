c54ac22c9664f0694893032faff028dd213fb272
ravi <ravikumarmamidi17@gmail.com>
2025-12-13 19:49:39 +0530

    resolve build error

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   eslint: {
-    ignoreDuringBuilds:true,
-
+    ignoreDuringBuilds: true,
   },
-  typescript:{
-    ignoreBuildErrors:true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
-}
+};
 
 export default nextConfig;
