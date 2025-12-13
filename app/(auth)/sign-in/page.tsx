50eb898ed269780e4341d3316759b7b575fcd4e3
ravi <ravikumarmamidi17@gmail.com>
2025-12-13 23:43:13 +0530

    signinpage client seperation

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,77 +1,8 @@
-'use client';
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
-import { useForm } from 'react-hook-form';
-import { Button } from '@/components/ui/button';
-import InputField from '@/components/forms/InputField';
-import FooterLink from '@/components/forms/FooterLink';
-// import {signInWithEmail, signUpWithEmail} from "@/lib/actions/auth.actions";
-// import {toast} from "sonner";
-// import {signInEmail} from "better-auth/api";
-import {useRouter} from "next/navigation";
-import { signInWithEmail } from '@/lib/actions/auth.actions';
-import { toast } from 'sonner';
+import SignInClient from "./SignInClient";
 
-const SignIn = () => {
-    const router = useRouter()
-    const {
-        register,
-        handleSubmit,
-        formState: { errors, isSubmitting },
-    } = useForm<SignInFormData>({
-        defaultValues: {
-            email: '',
-            password: '',
-        },
-        mode: 'onBlur',
-    });
-
-    const onSubmit = async (data: SignInFormData) => {
-        try {
-            const result= await signInWithEmail(data);
-            console.log(result)
-         
-            console.log("sign in aythundi");
-            if(result.success) router.push('/');
-        } catch (e) {
-            toast.error('Sign in failed',{
-                description: e instanceof Error ? e.message : 'Failed to sign in.'
-            })
-            console.error(e);
-            
-        }
-    }
-
-    return (
-        <>
-            <h1 className="form-title">Welcome back</h1>
-
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-                <InputField
-                    name="email"
-                    label="Email"
-                    placeholder="john@gmail.com"
-                    register={register}
-                    error={errors.email}
-                    validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/ }}
-                />
-
-                <InputField
-                    name="password"
-                    label="Password"
-                    placeholder="Enter your password"
-                    type="password"
-                    register={register}
-                    error={errors.password}
-                    validation={{ required: 'Password is required', minLength: 8 }}
-                />
-
-                <Button  type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
-                    {isSubmitting ? 'Signing In' : 'Sign In'}
-                </Button>
-
-                <FooterLink text="Don't have an account?" linkText="Create an account" href="/sign-up" />
-            </form>
-        </>
-    );
-};
-export default SignIn;
+export default function SignInPage() {
+  return <SignInClient />;
+}
