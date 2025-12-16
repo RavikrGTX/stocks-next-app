d572d2d5d132b70e8328d2ed43b935e26edcc27e
ravi <ravikumarmamidi17@gmail.com>
2025-12-16 23:49:21 +0530

    resolve id name issues

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-  id: "stocks-gravity", // changed from signalist
+  id: "signalist", // changed from signalist
   ai: {
     gemini: {
       apiKey: process.env.GEMINI_API_KEY!,
