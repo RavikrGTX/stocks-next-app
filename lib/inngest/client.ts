cd7e29548863163b5b3e62030dcd958629cb804f
ravi <ravikumarmamidi17@gmail.com>
2026-01-04 15:42:20 +0530

    [FIX]: inngest messages

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,16 +1,6 @@
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-<<<<<<< HEAD
     id: 'signalist',
     ai: { gemini: { apiKey: process.env.GEMINI_API_KEY! }}
 })
-=======
-  id: "signalist", // changed from signalist
-  ai: {
-    gemini: {
-      apiKey: process.env.GEMINI_API_KEY!,
-    },
-  },
-});
->>>>>>> 367871f79e6f6e7c6714602bee49b9af3f02f131
